ee316f6e0af62a145cb2d4124f93782e38122f4e
Harshad <riswadkar.harshad@gmail.com>
2026-07-03 18:08:33 +0530

    Added dictionary function for faster result

--- arrays_and_hashing/two_sum/solution.py
+++ arrays_and_hashing/two_sum/solution.py
@@ -14,14 +14,27 @@ from typing import List
 
 
 class Solution:
+    
     def two_sum(self, nums: List[int], target: int) -> List[int]:
-        # TODO: implement your approach here
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
         pass
+    
+    def two_sum_optimized(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i in range(len(nums)):
+            partner = target - nums[i]
+            if partner in seen:
+                return [seen[partner], i]
+            seen[nums[i]] = i
+
 
 
 if __name__ == "__main__":
     sol = Solution()
     # Try these once you've written your solution:
-    print(sol.two_sum([2, 7, 11, 15], 9))   # expected [0, 1]
-    print(sol.two_sum([3, 2, 4], 6))        # expected [1, 2]
-    print(sol.two_sum([3, 3], 6))           # expected [0, 1]
+    print(sol.two_sum_optimized([2, 7, 11, 15], 9))   # expected [0, 1]
+    print(sol.two_sum_optimized([3, 2, 4], 6))        # expected [1, 2]
+    print(sol.two_sum_optimized([3, 3], 6))           # expected [0, 1]
